1140c6651f277a47cd0419feef9e8c3f90f73499
hossam ali <hossemedine@outlook.com>
2022-11-30 14:51:25 +0100

    add annimation

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,15 +1,139 @@
-const Work = () => {
-    return ( <div className="h-fit w-full overflow-auto bg-red z-30 ">
-                <h1 className=" w-full  text-white text-2xl mx-auto text-center">My work</h1>
+import Image from "next/image";
+import img2 from "../../public/img2.jpg";
+import { motion } from "framer-motion";
 
-                <div className=" flex flex-col  gap-5">
-                    <div className="w-full md:w-[550px] h-96 border-2"></div>
-                    <div className="w-full md:w-[550px] h-96 border-2"></div>
-                    <div className="w-full md:w-[550px] h-96 border-2"></div>
-                    <div className="w-full md:w-[550px] h-96 border-2"></div>
-                </div>
-                
-    </div> );
-}
- 
-export default Work;
+const WorkWrapper: React.FC<{ position: number }> = ({ position }) => {
+  const showImage = position < 300 || position > 360;
+  const showContent = position > 300 && position < 360;
+
+  const container = {
+    hidden: { opacity: 1 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.12,
+        delayChildren: 0.04 * i,
+        
+      },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      //y:0,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+        repeat: Infinity,
+        duration:1
+      },
+    },
+    hidden: {
+      opacity: 0,
+      //y:20,
+      y: -20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
+  return (
+    <div
+      className={` z-30 ${
+        showImage ? "h-full" : "h-fit"
+      }  w-full overflow-auto transition-all`}
+    >
+      {showContent && (
+        <div className=" bg-white text-black">
+          <h1 className=" mx-auto  w-full text-center text-2xl ">My work</h1>
+
+          <div className=" flex flex-col  gap-5">
+            <div className="h-96 w-full border-2 md:w-[550px]"></div>
+            <div className="h-96 w-full border-2 md:w-[550px]"></div>
+            <div className="h-96 w-full border-2 md:w-[550px]"></div>
+            <div className="h-96 w-full border-2 md:w-[550px]"></div>
+          </div>
+        </div>
+      )}
+
+      {showImage && (
+        <div className="item- relative flex h-full w-full justify-center ">
+          <Image src={img2.src} fill objectFit="cover" sizes="100" alt="Hero" />
+          <p className="absolute  top-2 max-w-max bg-white  p-2 text-center text-6xl font-bold text-[#2e026d]">
+            My work
+          </p>
+          <motion.div variants={container} initial='hidden' animate='visible' className=" absolute top-20   ">
+            
+              <motion.svg
+                variants={child}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-20 w-20  text-red-800  "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </motion.svg>
+
+              <motion.svg
+                variants={child}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className=" top-10 h-20 w-20 text-red-800 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </motion.svg>
+
+              <motion.svg
+                variants={child}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="absolute top-10 h-20 w-20 text-red-800  "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </motion.svg>
+           
+          </motion.div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WorkWrapper;
+
+/*
+style={{
+            backgroundImage: `url(${img2.src})`,
+      
+            backgroundSize:'cover' ,
+            backgroundPosition: 'center', 
+            backgroundRepeat:'no-repeat',
+            
+            
+            }} 
+*/
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,80 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import Header from "../components/header";
-import { useAnimation, useScroll,motion, MotionValue } from "framer-motion"
+import { useScroll, motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import Work from "../components/work";
+import WorkWrapper from "../components/work";
+
 const Home: NextPage = () => {
-  const {scrollYProgress,scrollY}=useScroll()
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.12,
+        delayChildren: 0.04 * i,
+      },
+    }),
+  };
 
+  const child = {
+    visible: {
+      opacity: 1,
+      //y:0,
+      x: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      //y:20,
+      x: -20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
 
+  const text1 = "Hi!".split(" ");
+  const text2 = "My name is Hossem edine ali ".split(" ");
+  const text3 = "I am a web developper".split(" ");
 
-const y=useAnimation()
+  const { scrollY } = useScroll();
 
-const [top,setTop]=useState<number>(0)
-const [progress,setProgress]=useState(0.4)
-const [position,setPosition]=useState(0)
+  const [top, setTop] = useState<number>(0);
+  const [progress, setProgress] = useState(0.4);
+  const [position, setPosition] = useState(0);
 
+  useEffect(() => {
+    console.log("scrollYY", scrollY.get());
 
-  useEffect(()=>{
-   
-    scrollY.onChange(()=>{
-    if(scrollY.get()>500){
-      setPosition(1)
-    }
-    else{
-      setPosition(0)
-    }
-      if(scrollY.get()<320){
-        if((scrollY.get()/320)>0.4&&(scrollY.get()/320)<1){
-          setTop(scrollY.get())
-          setProgress(scrollY.get()/320)
-
+    scrollY.onChange(() => {
+      if (scrollY.get() > 500) {
+        setPosition(1);
+      } else {
+        setPosition(0);
+      }
+      if (scrollY.get() < 320) {
+        if (scrollY.get() / 320 > 0.4 && scrollY.get() / 320 < 1) {
+          setTop(scrollY.get());
+          setProgress(scrollY.get() / 320);
         }
-       
-
       }
 
-      if(scrollY.get()>360&&scrollY.get()<640){
-        if(scrollY.get()<640){
-          if(-1*(1-(640/scrollY.get()))>0.4){
-            setProgress(-1*(0.12+1-(640/scrollY.get())))
-
+      if (scrollY.get() > 360 && scrollY.get() < 640) {
+        if (scrollY.get() < 640) {
+          if (-1 * (1 - 640 / scrollY.get()) > 0.4) {
+            setProgress(-1 * (0.12 + 1 - 640 / scrollY.get()));
           }
-          
         }
-      } 
-
-      
-    })
-  },[scrollY.get()])
-
-
-
-
-
+      }
+    });
+  }, [scrollY.get()]);
 
   return (
     <>
@@ -63,21 +84,104 @@ const [position,setPosition]=useState(0)
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="min-h-[800px]   bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <Header/>
+        <Header />
 
         <div className="relative h-screen     ">
-          <div className="w-full px-10 md:w-1/2 h-[200px] md:py-10 ">
-                <p className="text-white text-xl md:text-2xl lg:text-3xl font-bold">Hi!</p>
-                <p className="text-white text-xl md:text-2xl lg:text-3xl font-bold">I am Hossem edine ali</p>
-                <p className="text-white text-xl md:text-2xl lg:text-3xl font-bold">A web developper</p>
-          </div>
-            {/*   */}
-         {true&& <motion.div  style={{scale:`${+progress.toString()}` ,  } }
+          <div className="h-[200px] w-full pl-5  md:w-1/2 md:py-10 md:pl-10 ">
+            <motion.p
+              className="flex flex-wrap overflow-hidden break-words"
+              variants={container}
+              initial="hidden"
+              animate="visible"
+            >
+              {text1.map((t, i) => {
+                return (
+                  <motion.span
+                    variants={child}
+                    key={i}
+                    className="mr-[5px] font-mono text-xl text-white hover:text-[#120a03] md:text-2xl md:font-bold"
+                  >
+                    {t}
+                  </motion.span>
+                );
+              })}
+            </motion.p>
 
-         className={` ${position==0?'fixed': 'absolute  '}   ${position==0?'top-0 right-0 left-0 bottom-0': 'top-[480px] right-0 left-0 bottom-[-380px]'}    z-10 border-2 ${progress>0.9?'overflow-auto':'overflow-hidden'} scrollbar-hide mt-10`}>
-              <Work/>
-          </motion.div>}
-         
+            <motion.p
+              className="flex flex-wrap overflow-hidden break-words"
+              variants={container}
+              initial="hidden"
+              animate="visible"
+            >
+              {text2.map((t, i) => {
+                return (
+                  <motion.span
+                    variants={child}
+                    key={i}
+                    className="mr-[5px] font-mono text-xl text-white hover:text-[#120a03] md:text-2xl md:font-bold"
+                  >
+                    {t}
+                  </motion.span>
+                );
+              })}
+            </motion.p>
+
+            <motion.p
+              className="flex flex-wrap overflow-hidden break-words"
+              variants={container}
+              initial="hidden"
+              animate="visible"
+            >
+              {text3.map((t, i) => {
+                return (
+                  <motion.span
+                    variants={child}
+                    key={i}
+                    className="mr-[6px] font-mono text-xl text-white transition-all duration-150 hover:text-red-900 md:text-2xl md:font-bold"
+                  >
+                    {t}
+                  </motion.span>
+                );
+              })}
+            </motion.p>
+          </div>
+          {/*   */}
+          {false && (
+            <motion.div
+              initial={{ opacity: 0, y: 25 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1 }}
+              style={{
+                scaleX: `${+progress.toString()}`,
+                scaleY: `${+progress.toString()}`,
+              }}
+              className={` ${position == 0 ? "fixed" : "absolute  "}   ${
+                position == 0
+                  ? "top-0 right-0 left-0 bottom-0"
+                  : "top-[480px] right-0 left-0 bottom-[-380px]"
+              }    z-10 border-2 ${
+                progress > 0.9 ? "overflow-auto" : "overflow-hidden"
+              } mt-10 rounded-2xl scrollbar-hide  `}
+            >
+              <WorkWrapper position={scrollY.get()} />
+            </motion.div>
+          )}
+
+<motion.div
+              initial={{ opacity: 0, y: 25 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1 }}
+              style={{
+                scaleX: `${+progress.toString()}`,
+                scaleY: `${+progress.toString()}`,
+              }}
+              className={`fixed top-0 right-0 left-0 bottom-0  border-2 ${
+                progress > 0.9 ? "overflow-auto" : "overflow-hidden"
+              } mt-10 rounded-2xl scrollbar-hide  `}
+            >
+              <WorkWrapper position={scrollY.get()} />
+            </motion.div>
+
           {/*   <motion.div 
             
             className={`flex   md:hidden  border-2  max-w-full max-h-screen mx-auto ${top<269?'overflow-hidden':'overflow-scroll'}  scrollbar-hide ` }
@@ -86,19 +190,14 @@ const [position,setPosition]=useState(0)
             {<Work/>}
             </motion.div> */}
 
-
-
-
-            {/* <div          
+          {/* <div          
               className={` md:flex   hidden  border-2  max-w-full max-h-screen mx-auto ${top<269?'overflow-hidden':'overflow-scroll'} scrollbar-hide` }
               style={{top:top+50,width:(top+300)*2,height:(top+90)*2}}
               >
                 {<Work/>}
             </div> */}
-            </div>
-          <div className="h-[1500px] border-2 mt-[350px] bg-red-600">
-
         </div>
+        <div className="mt-[350px] h-[1500px] border-2 "></div>
       </main>
     </>
   );
