e6413fe0be23924a0e1106a2e5dcc4a436b2a22e
hossemedineali <hossemedine@outlook.com>
2022-12-27 10:06:49 +0100

    testing new section 1

--- src/pages/t.tsx
+++ src/pages/t.tsx
@@ -1,13 +1,13 @@
 import  CustomCursor  from "../components/cursor/cursor";
 import Canvas from "../components/cursor/cursor";
-import CanvasSmoke from "../components/cursor/smoke";
+
 import Sphere from "../components/test/test";
 
 const Tedt = () => {
   return ( <div className=" min-h-screen fixed top-0 left-0 right-0 bottom-0">
      
    
-    <CanvasSmoke/>
+    <Sphere/>
   </div> );
 }
  
